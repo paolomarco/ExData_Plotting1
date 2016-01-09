ac2bcda3217ae91ba190204d21f37915dd3c8cc9
Tobia Martens <tobiamartens2016@u.northwestern.edu>
2016-01-08 21:34:16 -0800

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,35 @@
+path<-getwd()
+setwd(path)
+
+consumption_data<-read.table("household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?",nrows = 100000)
+
+consumption_data$DT<-paste(as.Date(consumption_data$Date,format = "%d/%m/%Y"),consumption_data$Time,sep = ' ')
+
+consumption_data$DT<-strptime(consumption_data$DT,format = "%Y-%m-%d %H:%M:%S")
+
+lower_bound<-"2007-01-31 23:59:00"
+upper_bound<-"2007-02-03 0:00:00"
+
+lower_bound<-strptime(lower_bound,format = "%Y-%m-%d %H:%M:%S")
+upper_bound<-strptime(upper_bound,format = "%Y-%m-%d %H:%M:%S")
+
+consumption_data<-subset(consumption_data,DT>lower_bound)
+consumption_data<-subset(consumption_data,DT<upper_bound)
+
+png(file= "plot4.png",width = 480, height = 480)
+par(mfrow=c(2,2))
+with(consumption_data,plot(DT,Global_active_power,col='white',xlab = "",ylab = "Global Active Power"))
+lines(consumption_data$DT,consumption_data$Global_active_power)
+
+with(consumption_data,plot(DT,Voltage,col='white',xlab = "dateTime",ylab = "Voltage"))
+lines(consumption_data$DT,consumption_data$Voltage)
+
+with(consumption_data,plot(DT,Sub_metering_1,col='white',xlab="",ylab="Energy Sub Metering"))
+lines(consumption_data$DT,consumption_data$Sub_metering_1)
+lines(consumption_data$DT,consumption_data$Sub_metering_2,col='red')
+lines(consumption_data$DT,consumption_data$Sub_metering_3,col='blue')
+legend("topright",c("Sub metering 1","Sub metering 2","Sub metering 3"), pch = c(45,45,45),col = c('black','red','blue'))
+
+with(consumption_data,plot(DT,Global_reactive_power,col='white',xlab = "dateTime",ylab = "Global Reactive Power"))
+lines(consumption_data$DT,consumption_data$Global_reactive_power)
+dev.off()
